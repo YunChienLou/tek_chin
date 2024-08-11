7d96cb8e06493ab8a9f337b8b16a6fef796ac7de
yun-chien Lou <ryan58147@gmail.com>
2024-08-12 04:52:52 +0800

    update @Repository

--- src/main/java/ryanlou/production/tek_chin/carousel/CarouselRepository.java
+++ src/main/java/ryanlou/production/tek_chin/carousel/CarouselRepository.java
@@ -3,9 +3,11 @@ package ryanlou.production.tek_chin.carousel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CarouselRepository extends JpaRepository<Carousel, Integer> {
     List<Carousel> findByEnable(boolean enable);
 
--- src/main/java/ryanlou/production/tek_chin/post/PostRepository.java
+++ src/main/java/ryanlou/production/tek_chin/post/PostRepository.java
@@ -3,9 +3,11 @@ package ryanlou.production.tek_chin.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByEnable(boolean enable);
 
